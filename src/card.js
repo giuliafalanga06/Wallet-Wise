185b80f52dca0dfc5a582f56883108100f881059
giuliafalanga06 <giulia01042006@gmail.com>
2025-01-19 18:57:46 +0100

    menu

--- src/card.js
+++ src/card.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-    
     for(var i = 0; i<6; i++){
-        $("#cards").append("<div class='card'><div class='card__img'><img src='https    ://via.placeholder.com/150' alt='card image'></div><div class='card__content'><h3 class='card__title'>Card Title</h3><p class='card__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quos.</p><a href='#' class='btn btn--block card__btn'>Read More</a></div></div>");
+        $("#cards").append("<div class='credit-card'><div class='card-header'><div class='chip'></div><div class='logo'>Wise</div></div><div class='card-number'>1234 5678 9012 3456</div><div class='card-footer'><div class='card-holder'><span>Titolare</span><p>Mario Rossi</p></div><div class='expires'><span>Scadenza</span><p>12/25</p></div></div></div>");
     }
   });
+
+  
