4defdf6fbe3871e6aace902ca98dc190aaf1d744
giuliafalanga06 <giulia01042006@gmail.com>
2025-01-27 23:36:02 +0100

    cards menu

--- src/card.js
+++ src/card.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     for(var i = 0; i<6; i++){
-        $("#cards").append("<div class='credit-card'><div class='card-header'><div class='chip'></div><div class='logo'>Wise</div></div><div class='card-number'>1234 5678 9012 3456</div><div class='card-footer'><div class='card-holder'><span>Titolare</span><p>Mario Rossi</p></div><div class='expires'><span>Scadenza</span><p>12/25</p></div></div></div>");
+        var id = "card"+i;
+        $(".cards").append("<div class='credit-card' id="+ id +"><div class='card-header'><div class='chip'></div><div class='logo'>Wise</div></div><div class='card-number'>1234 5678 9012 3456</div><div class='card-footer'><div class='card-holder'><span>Titolare</span><p>Mario Rossi</p></div><div class='expires'><span>Scadenza</span><p>12/25</p></div></div></div>");
+        $(".cardsSlider-nav").append("<a href=" + "#" + id+"></a>");
     }
   });
 
